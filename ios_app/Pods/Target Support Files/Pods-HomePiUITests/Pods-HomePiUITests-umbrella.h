6add6afd666d7c773ba1070adcedc73b74f7abbb
Viktor Kynchev <v.kynchev@gmail.com>
2016-11-20 14:46:45 +0200

    Moved iOS app to a different repo

--- ios_app/Pods/Target Support Files/Pods-HomePiUITests/Pods-HomePiUITests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_HomePiUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_HomePiUITestsVersionString[];
-
